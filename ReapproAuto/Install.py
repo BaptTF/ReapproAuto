9a87333b64da404cec3e78edddd9b54e9def647a
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2024-04-05 01:25:20 +0200

    Update installation script and README.md

--- ReapproAuto/Install.py
+++ ReapproAuto/Install.py
@@ -4,16 +4,21 @@ from pathlib import Path
 def install():
     if not os.path.exists(Path(f"{os.path.dirname(__file__)}/.env")):
         with open(Path(f"{os.path.dirname(__file__)}/.env"), "w") as env:
-            env.write(f"""MONGO_MDP={input("Enter your MongoDB password: ")}
-                        MONGO_PEM={input("Enter your abs path to MongoDB PEM file: ")}
-                        NUMERO_CARTE_PROMOCASH={input("Enter your Promocash card number: ")}
-                        PASSWORD_PROMOCASH={input("Enter your Promocash password: ")}
-                        IDENTIFIANT_AUCHAN={input("Enter your Auchan identifier: ")}
-                        PASSWORD_AUCHAN={input("Enter your Auchan password: ")}
-                        EMAIL={input("Enter your email in prenom.nom@telecomnancy.net: ")}
-                        WEB_BROWSER=firefox
-                        SEUIL_COURSE=50"""
+            ans = input("Enter the threshold for the course (Optionnal) (press ENTER to skip): ")
+            if not ans.isdigit():
+                ans = 50
+            env.write(f"""MONGO_MDP={input("Enter your MongoDB password (Obligatory): ")}
+                        MONGO_PEM={input("Enter your absolute path to MongoDB PEM file (Obligatory): ")}
+                        NUMERO_CARTE_PROMOCASH={input("Enter your Promocash card number (Optionnal) (press ENTER to skip): ")}
+                        PASSWORD_PROMOCASH={input("Enter your Promocash password (Optionnal) (press ENTER to skip): ")}
+                        IDENTIFIANT_AUCHAN={input("Enter your Auchan identifier (Optionnal) (press ENTER to skip): ")}
+                        PASSWORD_AUCHAN={input("Enter your Auchan password (Optionnal) (press ENTER to skip): ")}
+                        EMAIL={input("Enter your email in prenom.nom@telecomnancy.net (Obligatory): ")}
+                        WEB_BROWSER={"chrome" if input("Enter your web browser (Obligatory) (firefox/chrome):") == "chrome" else "firefox"}
+                        SEUIL_COURSE={ans}"""
                     )
-            print(f"The parameter are in {os.path.dirname(__file__)}/.env file")
+            print(f"The parameters are in {os.path.dirname(__file__)}/.env file")
+            print("If you want to change them, you can edit the file")
     else:
-        print(f"The parameter are in {os.path.dirname(__file__)}/.env file")
+        print(f"The parameters are in {os.path.dirname(__file__)}/.env file")
+        print("If you want to change them, you can edit the file")
